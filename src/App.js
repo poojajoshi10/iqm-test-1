35186ef264dded5a3262b6634f38818cc2bdfb8f
poojajoshi10 <poojajoshi460@gmail.com>
2021-02-21 21:10:05 +0530

    added loader, dialogue fix, scroll data load repeatation fixed

--- src/App.js
+++ src/App.js
@@ -11,49 +11,68 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Card from '@material-ui/core/Card';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import makeData from "./makeData";
 
 const Styles = styled.div`
   padding: 1rem;
 
-  table {
+  Table {
     border-spacing: 0;
-    border: 1px solid black;
+    border: 1px groove #d0d5db;
+  }
+  TableContainer {
+    border-spacing: 0;
+    border: 1px groove #d0d5db;
+  }
 
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
+  InfiniteScroll {
+    overflow-y: scroll;
+    height:100px;
+    width: 100%;
+    display:block;
   }
 `;
 
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: 'white',
+    fontWeight: 'bold',
+    color: theme.palette.common.black,
+  },
+  body: {
+    fontSize: 14
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+
 const styles = (theme) => ({
   root: {
-    margin: 0,
-    padding: theme.spacing(2),
+    margin: 2,
+    padding: theme.spacing(2)
   },
   closeButton: {
     position: 'absolute',
     right: theme.spacing(1),
     top: theme.spacing(1),
     color: theme.palette.grey[500],
-  },
+  }
 });
 
 const DialogTitle = withStyles(styles)((props) => {
@@ -84,11 +103,8 @@ const DialogActions = withStyles((theme) => ({
 }))(MuiDialogActions);
 
 // Function to create table component which use data of questions to show scroll infinite
-function Table({ columns, data, update }) {
+function CustomTable({ columns, data, update, stopLoader }) {
   // Use the state and functions returned from useTable to build your UI
-  const [modalShow, setModalShow] = React.useState(false);
-
-
   const {
     getTableProps,
     getTableBodyProps,
@@ -103,6 +119,13 @@ function Table({ columns, data, update }) {
     },
     useSortBy
   );
+const [title, setTitle] = React.useState('');
+const [author, setAuthor] = React.useState('');
+const [date, setDate] = React.useState('');
+const [views, setViews] = React.useState('');
+const [score, setScore] = React.useState('');
+const [link, setLink] = React.useState('');
+
 
   React.useEffect(() => {
     console.log("sort");
@@ -112,29 +135,37 @@ function Table({ columns, data, update }) {
   const [currentIndex, setIndex] = React.useState(0);
 
   const handleClickOpen = (index) => {
-    console.log('Current function', index);
+    console.log(rows[index]);
     setOpen(true);
     setIndex(index || 0);
+    setTitle(rows[index].original.title);
+    setAuthor(rows[index].values.author);
+    setLink(rows[index].original.link);
+    setDate(rows[index].original.date);
+    setViews(rows[index].original.views);
+    setScore(rows[index].original.score);
   };
   const handleClose = () => {
     setOpen(false);
   };
 
+
   // Render the UI for your table
   return (
     <>
-    <InfiniteScroll
+    <InfiniteScroll 
       dataLength={rows.length}
       next={update}
       hasMore={true}
-      loader={<h4>Loading more 2 items...</h4>}
+      loader={ !stopLoader ? <><LinearProgress /><LinearProgress color="secondary" /></>: <></>}
     >
-      <table {...getTableProps()}>
-        <thead>
+      <TableContainer component={Card} >
+      <Table {...getTableProps()} size="small">
+        <TableHead id='tableHead'>
           {headerGroups.map(headerGroup => (
-            <tr  {...headerGroup.getHeaderGroupProps()}>
+            <TableRow key="table-row-head"  {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                <StyledTableCell id={column} {...column.getHeaderProps(column.getSortByToggleProps())} align="left">
                   {column.render("Header")}
                   <span>
                     {column.isSorted
@@ -143,60 +174,61 @@ function Table({ columns, data, update }) {
                         : " 🔼"
                       : ""}
                   </span>
-                </th>
+                </StyledTableCell>
               ))}
-            </tr>
+            </TableRow>
           ))}
-        </thead>
+        </TableHead>
 
-        <tbody {...getTableBodyProps()}>
+        <TableBody {...getTableBodyProps()}>
           {rows.map((row, i) => {
-            console.log(row);
             prepareRow(row);
             return (<>
-              <tr id={rows[currentIndex].values.title} {...row.getRowProps()} onClick={()=> handleClickOpen(i)}>
+              <StyledTableRow key={rows[currentIndex].values.title} id={rows[currentIndex].values.title} {...row.getRowProps()} onClick={()=> handleClickOpen(i)}>
                 {row.cells.map(cell => {
                   return (
                     <>
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    <TableCell {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>
                     </>
                   );
                 })}
-              </tr>
-              <Dialog id={rows[i].original.title} onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-              <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-              <b>{rows[currentIndex].original.title}</b>
-              </DialogTitle>
-              <DialogContent dividers>
-                <Typography gutterBottom>
-                  
-                <b>Author:</b> {rows[currentIndex].values.author}
-                </Typography>
-                <Typography gutterBottom>
-                <b>On</b> : {rows[currentIndex].values.date ? rows[currentIndex].values.date: ''} 
-                </Typography>
-                <Typography gutterBottom>
-                  <b>Score</b>: {rows[currentIndex].original.score} 
-                </Typography>
-                <Typography gutterBottom>
-                <b>Total views:</b> {rows[currentIndex].original.views} 
-                </Typography>
-                <Typography gutterBottom>
-                <b>Link to answer:</b> <a href={rows[currentIndex].original.link}> {rows[currentIndex].original.link}</a>
-                </Typography>
-      
-              </DialogContent>
-              <DialogActions>
-                <Button autoFocus onClick={handleClose} color="primary">
-                  OK
-                </Button>
-              </DialogActions>
-              </Dialog>
+              </StyledTableRow>
               </>
             );
           })}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
+      </TableContainer>
+
+      <Dialog key='popup-model' onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+        <b>{title}</b>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            
+          <b>Author:</b> {author}
+          </Typography>
+          <Typography gutterBottom>
+          <b>On</b> : {date} 
+          </Typography>
+          <Typography gutterBottom>
+            <b>Score</b>: {score} 
+          </Typography>
+          <Typography gutterBottom>
+          <b>Total views:</b> {views} 
+          </Typography>
+          <Typography gutterBottom>
+          <b>Link to answer:</b> <a href={link} target="_blank"> {link}</a>
+          </Typography>
+
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            OK
+          </Button>
+              </DialogActions>
+              </Dialog>
     </InfiniteScroll>
     </>
   );
@@ -204,13 +236,14 @@ function Table({ columns, data, update }) {
 
  function App() {
   const [items, setItems] =  useState([]);
+  const [question, setQuestions] = React.useState([]);
+  const [endItemIndex, setEndItemIndex] = React.useState(0);
+
 
   useEffect(()=>{
     fetch('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
     .then((response)=> response.json())
     .then((resp)=>{
-        // console.log('ITEMS prepared'makeData(resp.items, 10));
-        console.log("*******@@@@@@@@@@@@@@@@@@@@@@@",resp.items)
         let all_questions=[]
         for(let i =0 ; i<resp.items.length ; i++)
         {
@@ -226,9 +259,9 @@ function Table({ columns, data, update }) {
         }
         all_questions.push(obj)
       }
-      console.log('My all question===== ', all_questions);
-        setItems(all_questions);
-        // setItems(makeData(resp.items, 10));
+        setQuestions(all_questions);
+        setItems([]);
+        setItems(all_questions.filter((q, i)=> i < 19));
       })
   }, []);
   
@@ -253,17 +286,20 @@ function Table({ columns, data, update }) {
   );
 
   const fetchMoreData = () => {
+    console.log('Updating data ....');
     setTimeout(() => {
-      setItems(items.concat(makeData(items, 2)));
+      setItems(makeData(question, items.length + 2));
     }, 1500);
   };
 
   const data = React.useMemo(() => items, [items]);
 
   return (
+    <>
     <Styles>
-      <Table columns={columns} data={data} update={fetchMoreData} />
+      <CustomTable key="custom-table" columns={columns} data={data} update={fetchMoreData}  stopLoader={question.length <= items.length}/>
     </Styles>
+  </>
   );
 }
 
